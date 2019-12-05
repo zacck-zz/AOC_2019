1efb9a15360de3d9a1a2f41346c3d9bb1a4b3ae9
Zacck Osiemo <coderv63@gmail.com>
2019-12-05 15:27:19 +0200

    inefficiently compute the closest intersection in terms of steps taken to achieve it

--- day_3/closest_intersection.cpp
+++ day_3/closest_intersection.cpp
@@ -7,7 +7,7 @@
 #include <cstdlib>
 using namespace std;
 
-vector< tuple<int, int> > plot_ordinates(vector<string> scales);
+vector< tuple<int, int, int> > plot_ordinates(vector<string> scales);
 
 int main() {
 	vector<string> first_scales;
@@ -29,11 +29,13 @@ int main() {
 
 	cout << "Input Read" << endl;
 
-	vector<tuple<int, int>> first_positions = plot_ordinates(first_scales);
-	vector<tuple<int, int>> second_positions = plot_ordinates(second_scales);
+	vector<tuple<int, int, int>> first_positions = plot_ordinates(first_scales);
+	vector<tuple<int, int, int>> second_positions = plot_ordinates(second_scales);
 
 	vector<int> neighbours;
-
+	vector<int> meet_sums;
+	vector<tuple<int, int, int>> first_intersections;
+	vector<tuple<int, int, int>> second_intersections;
 	for(auto f : first_positions){
 		for(auto s : second_positions){
 			int x1, x2, y1, y2;
@@ -43,9 +45,23 @@ int main() {
 			y2 = get<1>(s);
 			if(x1 == x2 && y1 == y2){
 					neighbours.push_back(abs(x1 + y1));
+					first_intersections.push_back(f);
+					second_intersections.push_back(s);
 			}
 		}
 	}
+
+	for(int i = 0; i < first_intersections.size(); i++){
+		int steps1 = get<2>(first_intersections[i]);
+		int steps2 = get<2>(second_intersections[i]);
+		meet_sums.push_back(steps1 + steps2);
+	}
+
+
+	sort(meet_sums.begin(), meet_sums.end());
+	cout << "Least steps is: " << meet_sums[0] << " steps away" << endl;
+
+
 
 	sort(neighbours.begin(), neighbours.end());
 	cout << "Closest Neighbour is: " << neighbours[0] << " away" << endl;
@@ -55,47 +71,53 @@ int main() {
 
 
 
-vector<tuple<int, int>> plot_ordinates(vector<string> scales) {
-	vector<tuple<int, int>> coordinates;
+vector<tuple<int, int, int>> plot_ordinates(vector<string> scales) {
+	vector<tuple<int, int, int>> coordinates_and_steps;
 
-	tuple<int, int> start_position = make_tuple(0,0);
+	tuple<int, int, int> start_position = make_tuple(0,0,0);
 
 	for(string s :  scales) {
 		string direction = s.substr(0,1);
 		istringstream stream(s.substr(1, s.size()));
 		int magnitude, x, y;
 	 	stream >> magnitude;
+		int steps = get<2>(start_position);
 		if(direction == "R") {
 				x = get<0>(start_position);
 				y = get<1>(start_position);
 				for(int k = 1; k <= magnitude; k++){
-					start_position = make_tuple(x + k, y);
-					coordinates.push_back(start_position);
+					steps += 1;
+					start_position = make_tuple(x + k, y, steps);
+					coordinates_and_steps.push_back(start_position);
 				}
 		 } else if (direction == "L") {
 				x = get<0>(start_position);
 				y = get<1>(start_position);
 				for(int k = 1; k <= magnitude; k++) {
-					start_position = make_tuple(x - k,y);
-					coordinates.push_back(start_position);
+					steps +=1;
+					start_position = make_tuple(x - k,y,steps);
+					coordinates_and_steps.push_back(start_position);
 				}
 			} else if (direction == "U") {
 				x = get<0>(start_position);
 				y = get<1>(start_position);
 				for(int k = 1; k <= magnitude; k++){
-					start_position = make_tuple(x, y + k);
-					coordinates.push_back(start_position);
+					steps +=1;
+					start_position = make_tuple(x, y + k, steps);
+					coordinates_and_steps.push_back(start_position);
 				}
 			} else if (direction == "D") {
 				x = get<0>(start_position);
 				y = get<1>(start_position);
 				for(int k = 1; k <= magnitude; k++){
-			 	  start_position = make_tuple(x, y - k);
-				  coordinates.push_back(start_position);
+					steps +=1;
+			 	  start_position = make_tuple(x, y - k, steps);
+				  coordinates_and_steps.push_back(start_position);
 				}
 		}
+
 	}
-	return coordinates;
+	return coordinates_and_steps;
 }
 
 
